141d8f53db9e397e018fe4c3235337bbe2ddf8d2
John Robling <roblingjohn@gmail.com>
2020-07-20 13:46:20 -0400

    added event listeners for buttons and simplified jquery script tags

--- public/js/signup.js
+++ public/js/signup.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
     })
       .then(function(data) {
         // console.log(data);
-        window.location.replace("/grocery-list");
+        window.location.replace("/upload");
         // If there's an error, handle it by throwing up a bootstrap alert
       })
       .catch(handleLoginErr);
