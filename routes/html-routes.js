c579c8bdfe4b0ad12a9e77b0a99c0a94b6a5fdfe
Crystal <crystalwatkins428@gmail.com>
2020-07-20 11:26:38 -0400

    help

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,4 +1,5 @@
 // Requiring path to so we can use relative routes to our HTML files
+var express = require("express")
 var path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
@@ -9,23 +10,23 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/grocery-list");
+      res.render("grocerylist");
     }
-    res.sendFile(path.join(__dirname, "../public/home-page.html"));
+    res.render(path.join(__dirname, "index"));
   });
 
   app.get("/signup", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/grocery-list");
+      res.redirect("grocerylist");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.sendFile(path.join(__dirname, "signup"));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/grocery-list", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/grocery-list.html"));
+  app.get("/grocerylist", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "grocerylist"));
   });
 
 };
