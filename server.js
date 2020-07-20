16523c7e8fed92a0ecd069a0f084f2ee5235f442
Crystal <crystalwatkins428@gmail.com>
2020-07-20 10:41:21 -0400

    working webpage

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var session = require("express-session");
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
+var exphbs = require("express-handlebars");
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
