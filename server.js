6cda0b20a8375043ab014db5c7fcfc68ddd25272
Crystal <crystalwatkins428@gmail.com>
2020-07-21 10:42:27 -0400

    update

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ require("./controllers/html-routes.js")(app);
 require("./controllers/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
